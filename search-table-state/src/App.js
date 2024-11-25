7b8ee3b23e504ae51a43b69c92faafbd89c28ad6
Tammie Childs <80907597+tchilds55@users.noreply.github.com>
2024-11-24 19:18:19 -0500

    Update App.js
    Updating documentation

--- search-table-state/src/App.js
+++ search-table-state/src/App.js
@@ -2,7 +2,8 @@ import "./styles/global.css";
 import { Header } from "./components/Layout/Header/Header";
 import { Main } from "./components/Layout/Main/Main";
 
-// Search table state
+// Search table state app
+// This app creates a search table of data
 function App() {
   return (
     <div className="App">
