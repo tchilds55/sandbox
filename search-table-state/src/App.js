e1d0b0837570033ba58f0db49a5cd0d889ab0010
Tammie Childs <tammie.childs@shopify.com>
2024-11-24 19:19:39 -0500

    Added more documentation to App

--- search-table-state/src/App.js
+++ search-table-state/src/App.js
@@ -2,6 +2,8 @@ import "./styles/global.css";
 import { Header } from "./components/Layout/Header/Header";
 import { Main } from "./components/Layout/Main/Main";
 
+// Adding documentation here
+// This is an app to create a table with search functionality
 function App() {
   return (
     <div className="App">
