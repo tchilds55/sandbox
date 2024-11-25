7426d28628b57ebfcc023bb1796cf10e38dda18a
Tammie Childs <80907597+tchilds55@users.noreply.github.com>
2024-11-24 19:00:05 -0500

    Adding comments to App (#1)

--- search-table-state/src/App.js
+++ search-table-state/src/App.js
@@ -2,6 +2,7 @@ import "./styles/global.css";
 import { Header } from "./components/Layout/Header/Header";
 import { Main } from "./components/Layout/Main/Main";
 
+// Search table state
 function App() {
   return (
     <div className="App">
